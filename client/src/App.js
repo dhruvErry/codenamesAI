69705682272ba4981fbdc4127ba7d5ae7bd13ec7
Dhruv Erry <dhruv.erryde@gmail.com>
2025-07-04 16:01:05 -0700

    Test commit #2

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import { useState, useEffect } from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import JoinForm  from './components/JoinForm';
 import Game from './components/Game';
