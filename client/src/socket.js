aa00ff5b54da80c64e4b15218ed3ef88a544babd
Dhruv Erry <dhruv.erryde@gmail.com>
2025-08-07 14:05:23 -0700

    Delete files

--- client/src/socket.js
+++
@@ -1,8 +0,0 @@
-import { io } from 'socket.io-client';
-
-const serverURL = process.env.REACT_APP_SERVER_URL;
-const socket = io(serverURL);    
-
-// const socket = io('http://localhost:8080');
-
-export default socket;
